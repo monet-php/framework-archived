49c17798f170cd0cdc66d7231477591c35b37986
Kieron Sutton <sutton.kieron99@gmail.com>
2022-09-05 06:15:23 +0100

    WIP

--- src/Admin/Providers/AdminServiceProvider.php
+++ src/Admin/Providers/AdminServiceProvider.php
@@ -3,12 +3,17 @@
 namespace Monet\Framework\Admin\Providers;
 
 use Filament\PluginServiceProvider;
+use Monet\Framework\Admin\Filament\Pages\SiteSettings;
 use Monet\Framework\Admin\Filament\Resources\ModuleResource;
 use Monet\Framework\Admin\Filament\Resources\ThemeResource;
 
 class AdminServiceProvider extends PluginServiceProvider
 {
     public static string $name = 'monet-admin';
+
+    protected array $pages = [
+        SiteSettings::class
+    ];
 
     protected array $resources = [
         ModuleResource::class,
