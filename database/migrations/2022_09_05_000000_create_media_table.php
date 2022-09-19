9532afc14254dd3b33a7a5ed04963d380cc602db
Kieron Sutton <sutton.kieron99@gmail.com>
2022-09-20 00:18:10 +0100

    WIP

--- database/migrations/2022_09_05_000000_create_media_table.php
+++ database/migrations/2022_09_05_000000_create_media_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateMediaTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('media', function (Blueprint $table) {
@@ -30,15 +25,5 @@ class CreateMediaTable extends Migration
             $table->unsignedBigInteger('size')->nullable();
             $table->nullableTimestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('media');
     }
 }
--- database/migrations/2022_09_05_000000_create_permissions_tables.php
+++ database/migrations/2022_09_05_000000_create_permissions_tables.php
@@ -7,11 +7,6 @@ use Spatie\Permission\PermissionRegistrar;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up(): void
     {
         $tableNames = config('permission.table_names');
@@ -128,25 +123,5 @@ return new class extends Migration
         app('cache')
             ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
             ->forget(config('permission.cache.key'));
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down(): void
-    {
-        $tableNames = config('permission.table_names');
-
-        if (empty($tableNames)) {
-            throw new Exception('Error: config/permission.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.');
-        }
-
-        Schema::drop($tableNames['role_has_permissions']);
-        Schema::drop($tableNames['model_has_roles']);
-        Schema::drop($tableNames['model_has_permissions']);
-        Schema::drop($tableNames['roles']);
-        Schema::drop($tableNames['permissions']);
     }
 };
